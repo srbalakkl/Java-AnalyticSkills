3e055e06fb3f9bfd027e0441175a3a87fe46ed2d
srbalakkl <sr.bala.kkl@gmail.com>
2021-08-27 11:11:57 +0530

    add binary converter

---
+++ src/BinaryConverter/Binaryconverter.java
@@ -0,0 +1,23 @@
+package BinaryConverter;
+
+import java.util.Scanner;
+
+public class Binaryconverter {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the integer value : ");
+        int x = sc.nextInt();
+
+//        StringBuilder binary = new StringBuilder();
+        int rem;
+        String binary = "";
+        while (x >= 1) {
+            rem = x % 2;
+            binary = binary + rem;
+//            binary.append(rem);  <- use this statement to remove warning
+            x = x / 2;
+        }
+        System.out.println(binary);
+    }
+}
