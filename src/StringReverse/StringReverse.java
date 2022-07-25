d40b41c498e4547b7d8cdb0f28956c18bc2753d0
sr.bala <sr.bala.kkl@gmail.com>
2022-07-25 07:40:55 +0530

    DOC:StringReverse

--- src/StringReverse/StringReverse.java
+++ src/StringReverse/StringReverse.java
@@ -1,7 +1,22 @@
+/**
+
+Reverse a String
+
+Write a program to take a string as input and output its reverse.
+The given code takes a string as input and converts it into a char array, which contains letters of the string as its elements.
+
+Sample Input:
+hello there
+
+Sample Output:
+ereht olleh
+
+*/
+
+
 package StringReverse;
 
 import java.util.Scanner;
-
 public class StringReverse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
